65d600bbb049d5608cca674cb6e06c81b516bc33
tsukiroku <tsukiroku@yahoo.com>
2022-10-06 22:24:57 +0900

    Create Button.tsx

---
+++ src/components/components/Button.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from 'react'
+import { PropsBase } from '../index'
+
+interface Props extends PropsWithChildren<Partial<PropsBase>> {
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+    fontSize?: number | string
+}
+
+export default (props: Props) => (
+    <button className={`pr-2 pl-2 ${props.fontSize} focus:outline-none ${props.className ?? ''}`} onClick={props.onClick}>
+        {props.children}
+    </button>
+)
