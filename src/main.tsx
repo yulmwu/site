d4000d01302b984916e0d47f8503dfbcadc812cc
tsukiroku <tsukiroku@yahoo.com>
2022-10-06 22:24:50 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
-import Background from './components/Background'
-
-import * as Utilitiy from './utils'
-
+import * as Components from './components'
 import { ThemeProvider } from './context/theme'
 
 import '../global.css'
@@ -13,16 +9,21 @@ import '../global.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider>
-            <Background />
+            <Components.Background.default />
             <div className='absolute top-0 left-0 w-full bg-transparent select-none'>
                 <App inc={1} toggleButton />
                 <App inc={5} />
                 <App inc={10} />
-                <Utilitiy.Center>
-                    <Utilitiy.Text className='text-2xl sm:text-3xl'>
+
+                <Components.Center.default>
+                    <Components.Text.default className='text-2xl sm:text-3xl'>
                         <a href='https://github.com/ky0422/ky0422.github.io'>GITHUB</a>
-                    </Utilitiy.Text>
-                </Utilitiy.Center>
+                    </Components.Text.default>
+
+                    <Components.Text.default className='text-2xl sm:text-3xl'>
+                        <a href='https://ky0422.tistory.com'>TISTORY</a>
+                    </Components.Text.default>
+                </Components.Center.default>
             </div>
         </ThemeProvider>
     </React.StrictMode>
