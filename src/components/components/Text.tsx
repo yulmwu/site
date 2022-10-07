32890ef4ba394843209b02c14b36a6c11d7340c4
tsukiroku <tsukiroku@yahoo.com>
2022-10-07 19:59:43 +0900

    Update Text.tsx

--- src/components/components/Text.tsx
+++ src/components/components/Text.tsx
@@ -3,22 +3,24 @@ import { Theme, ThemeContext } from '../../context/theme'
 import { PropsBase } from '../index'
 
 interface Props extends Partial<PropsBase> {
-    className?: string
     fontSize?: number | string
     stroke?: boolean
     color?: string
+    uppercase?: boolean
 }
 
-export default ((props: Props) => {
+export default (props: Props) => {
     const { toggle } = React.useContext(ThemeContext) as Theme
+
+    const { fontSize, stroke, color, uppercase, className, children } = props
 
     return (
         <p
-            className={`${props.fontSize ?? 'text-9xl'} bg-transparent text-transparent ${
-                props.stroke ? (toggle ? 'text-stroke' : 'text-stroke-light') : props.color ?? (toggle ? 'text-white' : 'text-gray-800')
-            } ${props.className ?? ''}`}
+            className={`${fontSize ?? 'text-9xl'} bg-transparent text-transparent ${
+                stroke ? (toggle ? 'text-stroke' : 'text-stroke-light') : color ?? (toggle ? 'text-white' : 'text-gray-800')
+            } ${className ?? ''}`}
         >
-            {props.children}
+            {uppercase ? children?.toString().toUpperCase() : children}
         </p>
     )
-})
+}
