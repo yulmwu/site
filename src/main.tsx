10ecdc514bba02dc7d8ebc6d2c90b8004935ac93
tsukiroku <tsukiroku@yahoo.com>
2022-10-07 19:59:39 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,27 +9,27 @@ import '../global.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider>
-            <Components.Background.default />
+            <Components.Background />
             <div className='absolute top-0 left-0 w-full bg-transparent select-none'>
-                <Components.Center.default>
-                    <Components.Text.default className='text-4xl sm:text-6xl' stroke>
-                        다람쥐 헌 쳇바퀴에 타고파
-                    </Components.Text.default>
-                </Components.Center.default>
+                <Components.Center>
+                    <Components.Text className='text-4xl sm:text-6xl' uppercase>
+                        ky0422
+                    </Components.Text>
+                </Components.Center>
 
                 <App inc={1} toggleButton />
                 <App inc={5} />
                 <App inc={10} />
 
-                <Components.Center.default>
-                    <Components.Text.default className='text-2xl sm:text-3xl'>
+                <Components.Center>
+                    <Components.Text className='text-2xl sm:text-3xl'>
                         <a href='https://github.com/ky0422/ky0422.github.io'>GITHUB</a>
-                    </Components.Text.default>
+                    </Components.Text>
 
-                    <Components.Text.default className='text-2xl sm:text-3xl'>
+                    <Components.Text className='text-2xl sm:text-3xl'>
                         <a href='https://ky0422.tistory.com'>TISTORY</a>
-                    </Components.Text.default>
-                </Components.Center.default>
+                    </Components.Text>
+                </Components.Center>
             </div>
         </ThemeProvider>
     </React.StrictMode>
