a0d34a2bd8ac6ab5cd380cb2d60d9b8b524899d2
tsukiroku <tsukiroku@yahoo.com>
2022-10-07 20:00:08 +0900

    Update Center.tsx

--- src/components/components/Center.tsx
+++ src/components/components/Center.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { PropsBase } from '../index'
 import { Theme, ThemeContext } from '../../context/theme'
 
-export default (props: Partial<PropsBase>) => {
-    const { toggle } = React.useContext(ThemeContext) as Theme
-
-    return (
-        <div
-            className={`flex flex-col content-center justify-center text-center h-screen bg-transparent ${toggle ? 'text-white' : 'text-gray-800'} ${
-                props.className ?? ''
-            }`}
-        >
-            {props.children}
-        </div>
-    )
-}
+export default (props: Partial<PropsBase>) => (
+    <div
+        className={`flex flex-col content-center justify-center text-center h-screen bg-transparent ${
+            (React.useContext(ThemeContext) as Theme).toggle ? 'text-white' : 'text-gray-800'
+        } ${props.className ?? ''}`}
+    >
+        {props.children}
+    </div>
+)
