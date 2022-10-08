8133244eaedb36daf8fc2069ef068390d42a5f44
tsukiroku <tsukiroku@yahoo.com>
2022-10-08 15:36:14 +0900

    Update Background.tsx

--- src/components/components/Background.tsx
+++ src/components/components/Background.tsx
@@ -2,22 +2,22 @@ import React, { RefObject } from 'react'
 import VantaBirds from 'vanta/dist/vanta.birds.min'
 import { Theme, ThemeContext } from '../../context/theme'
 
-const birds = (birdsRef: RefObject<HTMLDivElement>) => {
-    if (birdsRef.current)
-        VantaBirds({
-            el: birdsRef.current,
-            backgroundAlpha: 0,
-            quantity: 3,
-            speedLimit: 3,
-            alignment: 10000,
-        })
-}
+const birds = (birdsRef: RefObject<HTMLDivElement>) =>
+    birdsRef.current &&
+    VantaBirds({
+        el: birdsRef.current,
+        backgroundAlpha: 0,
+        quantity: 3,
+        speedLimit: 3,
+        alignment: 10000,
+        cohesion: 0,
+    })
 
 export default () => {
     const { toggle } = React.useContext(ThemeContext) as Theme
     const birdsRef = React.useRef<HTMLDivElement>(null)
 
-    React.useEffect(() => birds(birdsRef), [])
+    React.useEffect(() => void birds(birdsRef), [])
 
     return <div className={`fixed w-[100vw] h-[100vh] ${toggle ? 'bg-background' : 'bg-background-light'}`} ref={birdsRef} />
 }
