e7aeafce5d9c851f35d01102fb84bc19e1834344
tsukiroku <tsukiroku@yahoo.com>
2022-10-09 20:12:24 +0900

    Update Title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,31 +1,37 @@
 import * as Components from './index'
-import useWebAnimations, { bounceInLeft, headShake } from '@wellyshen/use-web-animations'
+import useWebAnimations, { bounceInLeft, bounceInRight, headShake } from '@wellyshen/use-web-animations'
 
 export default () => {
-    const { ref, animate } = useWebAnimations<HTMLParagraphElement>({
-        ...bounceInLeft,
-        animationOptions: {
-            duration: 700,
-        },
-    })
+    const { ref: titleRef, animate: titleAnimate } = useWebAnimations<HTMLParagraphElement>(bounceInLeft)
+    const { ref: subtitleRef } = useWebAnimations<HTMLParagraphElement>(bounceInRight)
+
+    const title = (
+        <div ref={titleRef} onClick={() => titleAnimate(headShake)}>
+            <Components.Text className='text-4xl sm:text-6xl' uppercase>
+                ky0422
+            </Components.Text>
+        </div>
+    )
+
+    const [style, pr] = ['text-xl sm:text-2xl', 'pr-1.5']
+
+    const subtitle = (
+        <div className='pt-3' ref={subtitleRef}>
+            <Components.Text className={`${style} ${pr}`} color='text-blue-500'>
+                다람쥐
+            </Components.Text>
+            <Components.Text className={`${style} ${pr}`}>헌</Components.Text>
+            <Components.Text className={style} color='text-red-500'>
+                쳇바퀴
+            </Components.Text>
+            <Components.Text className={`${style} ${pr}`}>에 타고파</Components.Text>
+        </div>
+    )
 
     return (
         <div className='inline-block'>
-            <div ref={ref} onClick={() => animate(headShake)}>
-                <Components.Text className='text-4xl sm:text-6xl' uppercase>
-                    ky0422
-                </Components.Text>
-            </div>
-            <div className='pt-3'>
-                <Components.Text className='text-xl sm:text-2xl pr-1.5' color='text-blue-500'>
-                    다람쥐
-                </Components.Text>
-                <Components.Text className='text-xl sm:text-2xl pr-1.5'>헌</Components.Text>
-                <Components.Text className='text-xl sm:text-2xl' color='text-red-500'>
-                    쳇바퀴
-                </Components.Text>
-                <Components.Text className='text-xl sm:text-2xl'>에 타고파</Components.Text>
-            </div>
+            {title}
+            {subtitle}
         </div>
     )
 }
