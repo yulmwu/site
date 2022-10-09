f9f85f9aef476855400b512bf1cecf49163f7908
tsukiroku <tsukiroku@yahoo.com>
2022-10-09 20:12:25 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import * as Components from './components'
 import { ThemeProvider } from './context/theme'
+import { Fade } from 'react-awesome-reveal'
 
 import '../global.css'
 
@@ -11,22 +12,26 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <ThemeProvider>
             <Components.Background />
             <div className='absolute top-0 left-0 w-full bg-transparent select-none'>
-                <Components.Center>
-                    <Components.Title />
-                </Components.Center>
+                <Fade delay={300}>
+                    <Components.Center>
+                        <Components.Title />
+                    </Components.Center>
+                </Fade>
 
                 <App inc={1} toggleButton />
                 <App inc={5} />
                 <App inc={10} />
 
                 <Components.Center>
-                    <Components.Text className='text-2xl sm:text-3xl'>
-                        <a href='https://github.com/ky0422/ky0422.github.io'>GITHUB</a>
-                    </Components.Text>
+                    <Fade delay={300}>
+                        <Components.Text className='text-2xl sm:text-3xl'>
+                            <a href='https://github.com/ky0422/ky0422.github.io'>GITHUB</a>
+                        </Components.Text>
 
-                    <Components.Text className='text-2xl sm:text-3xl'>
-                        <a href='https://ky0422.tistory.com'>TISTORY</a>
-                    </Components.Text>
+                        <Components.Text className='text-2xl sm:text-3xl'>
+                            <a href='https://ky0422.tistory.com'>TISTORY</a>
+                        </Components.Text>
+                    </Fade>
                 </Components.Center>
             </div>
         </ThemeProvider>
