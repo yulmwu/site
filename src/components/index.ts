9abe0d77c0a7b34678312851a55ea60366407d25
tsukiroku <tsukiroku@yahoo.com>
2022-10-09 19:25:20 +0900

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,6 @@ export { default as Button } from './components/Button'
 export { default as Center } from './components/Center'
 export { default as Background } from './components/Background'
 
+export { default as Title } from './Title'
+
 export type PropsBase = PropsWithChildren<{ className: string }>
